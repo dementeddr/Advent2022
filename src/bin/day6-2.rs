aadaf722ab1181e8307b5f638da8ed8d812e26e2
dementeddr <2413901+dementeddr@users.noreply.github.com>
2022-12-06 14:08:13 -0500

    Day 6 done. String manipulation in rust is the worst

--- src/bin/day6-2.rs
+++ src/bin/day6-2.rs
@@ -4,24 +4,23 @@ use std::collections::HashSet;
 
 fn main() {
 
+    const UNIQUE: usize = 14;
+
     let args: Vec<String> = env::args().collect();
     let input_path = args.get(1).expect("Path to input file required");
     let input = fs::read_to_string(input_path).expect("Could't read input file");
 
-    for i in 0..input.len()-3 {
+    for i in 0..input.len()-UNIQUE+1 {
         
         let mut window_chars = HashSet::new();
-        let slice = input.get(i..i+4).unwrap();
+        let slice = input.get(i..i+UNIQUE).unwrap();
         
         for c in slice.chars() {
             window_chars.insert(c);
-            //print!("{c}");
         }
         
-        //println!("  {}", window_chars.len());
-
-        if window_chars.len() == 4 {
-            println!("Number of processed chars is {}", i+4);
+        if window_chars.len() == UNIQUE {
+            println!("Number of processed chars is {}", i+UNIQUE);
             break;
         }
 
