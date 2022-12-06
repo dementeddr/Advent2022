b638e61b7e2ed06600547df5d076dc80371faced
dementeddr <2413901+dementeddr@users.noreply.github.com>
2022-12-06 13:01:05 -0500

    Updated the template. Takes input path from command line args now

--- new-day.sh
+++ new-day.sh
@@ -1,4 +1,4 @@
 #! /bin/zsh
 
-curl -sSLk "https://adventofcode.com/2022/day/$1/input" > src/input/day$1-input.txt
 cp template.rs src/bin/day$1-1.rs
+curl -sSLk "https://adventofcode.com/2022/day/$1/input" > src/input/day$1-input.txt
--- template.rs
+++ template.rs
@@ -1,13 +1,17 @@
+use std::env;
 use std::fs;
 use std::str;
+use std::collections;
+use regex::Regex;
 
 fn main() {
 
-    let input_path = "../input.txt";
+    let args: Vec<String> = env::args().collect();
+    let input_path = args.get(1).expect("Path to input file required");
     let input = fs::read_to_string(input_path).expect("Could't read input file");
     let input: str::Split<&str> = input.split("\n");
 
     for line in input {
-
+    
     }
 }
